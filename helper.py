79a2ea7cd82109acbdc6cc1406372fb92abfb26a
oscar ou <oscaraugcc@gmail.com>
2019-11-25 15:57:17 -0500

    csv helpers

--- helper.py
+++ helper.py
@@ -29,3 +29,29 @@ def turn_record_to_sample(row_record):
 
     #return playerid, features, label
     return samples[0], samples[1:-1], samples[-1]
+
+# csv helpers
+def print_accuracy(iteration, accuracy, classification_type):
+    output = open("g19_DT_"+classification_type+"_accuracy.csv", 'w')
+    content = "Dataset number, Accuracy,\n"
+
+    i=0
+    if iteration == len(accuracy):
+        for a in accuracy:
+            content += str(i) + ", " + str(a) + ",\n"
+            i += 1
+
+        output.write(content)
+        output.close
+
+def print_prediction(iteration, classification, prediction, classification_type):
+    output = open("g19_DT_"+classification_type+"_predictions.csv", 'a')
+    content = ""
+
+    if iteration == 0:
+        content = "Iteration, Classification, Predictions\n"
+    for i in range(len(classification)):
+        content += str(iteration) + ", " + str(classification[i]) + ", " + str(prediction[u][0]) + ",\n"
+
+    output.write(content)
+    output.close
